e3c0c5a1658b383f3c9b30e0edc92576bb08ec2d
ArthurDenter <joerg.adomaitis@icloud.com>
2023-07-20 13:22:40 +0200

    add user input for new div grid dimension Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-const createInitialGridOfSquareDivs = function () {
+const createNewGridOFSquareDivs = function (numberOfSquareDivsPerSide) {
   const mainContainer = document.querySelector(".main-container");
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+  for (let i = 0; i < numberOfSquareDivsPerSide; i++) {
+    for (let j = 0; j < numberOfSquareDivsPerSide; j++) {
       const divBox = document.createElement("div");
       divBox.classList.add("divBox");
       mainContainer.appendChild(divBox);
       divBox.addEventListener("mouseover", () => {
         divBox.style.backgroundColor = "red";
       });
-      //   divBox.addEventListener("mouseout", ()=>{
-      //     divBox.style.backgroundColor = "black";
-      // });
+    };
+  };
+};
+
+const removeGridContainerFromCanvas = function () {
+  const gridDivs = document.querySelectorAll(".divBox");
+  gridDivs.forEach((gridDiv) => {
+    gridDiv.remove();
+  });
+};
+
+const getUserInput = function (callback) {
+  let newGridSize = window.prompt("Please enter a number(between 0 and 100) of squares per side for the new grid.");
+  if (isNaN(newGridSize)) {
+    alert("This is not a valid Number!")
+    getUserInput(createNewGridOFSquareDivs);
+  }
+  else if (newGridSize === null) {
+    return callback(16);
+  }
+  else {
+    if ((newGridSize > 0) && (newGridSize < 100)) {
+      callback(newGridSize);
+    }
+    else {
+      alert("Your given value is not between 1 and 100!");
+      getUserInput(createNewGridOFSquareDivs);
     }
   }
 };
 
 
-createInitialGridOfSquareDivs();
+
+//initial grid layout
+createNewGridOFSquareDivs(16);
+const redrawCanvasButton = document.querySelector(".redrawCanvasButton");
+redrawCanvasButton.addEventListener("click", () => {
+  removeGridContainerFromCanvas();
+  getUserInput(createNewGridOFSquareDivs);
+});
