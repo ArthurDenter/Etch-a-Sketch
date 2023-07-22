2db1ad2394f77747a4fff80dff4433478e5ec336
ArthurDenter <joerg.adomaitis@icloud.com>
2023-07-22 21:48:02 +0200

    add some styles, refactor createNewGridOFSquareDivs Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-const divBoxSize = function (numberOfSquareDivsPerSide, canvasSize, borderThickness) {
+const divBoxSize = function (numberOfSquareDivsPerSide) {
+  const borderThickness = 1;
+  const canvasSize = 500;
   return Math.floor(((canvasSize / numberOfSquareDivsPerSide) - (borderThickness * 2)) * 10) / 10;
 }
 
-const createNewGridOFSquareDivs = function (numberOfSquareDivsPerSide) {
-  const mainContainer = document.querySelector(".main-container");
-  for (let i = 0; i < Math.pow(numberOfSquareDivsPerSide, 2); i++) {
+const totalNumberOfSquareDivs = function (numberOfSquareDivsPerSide, callback) {
+  callback(Math.pow(numberOfSquareDivsPerSide, 2), divBoxSize(numberOfSquareDivsPerSide));
+};
+
+const changeDivBackground = function (event) {
+  switch (event.type) {
+    case "mouseover":
+      if (event.buttons > 0) {
+        // console.log("mouseover + click")
+        event.target.style.backgroundColor = "#ffffff";
+      };
+      break;
+    case "mousedown":
+      // console.log("mousedown");
+      // console.log(event);
+      event.target.style.backgroundColor = "#ffffff";
+  };
+}
+
+const createNewGridOFSquareDivs = function (totalNumberOfSquareDivs, divBoxSize) {
+  const mainContainer = document.querySelector(".canvas-container");
+  for (let i = 0; i < totalNumberOfSquareDivs; i++) {
     const divBox = document.createElement("div");
     divBox.classList.add("divBox");
     divBox.setAttribute("draggable", "false");
-    divBox.style.width = `${divBoxSize(numberOfSquareDivsPerSide, 500, 1)}px`;
-    divBox.style.height = `${divBoxSize(numberOfSquareDivsPerSide, 500, 1)}px`;
+    divBox.style.width = `${divBoxSize}px`;
+    divBox.style.height = `${divBoxSize}px`;
     mainContainer.appendChild(divBox);
-    divBox.addEventListener("mouseover", (eventMouseover) => {
-      console.log(eventMouseover);
-      if (eventMouseover.buttons > 0) {
-        divBox.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-      };
-    });
-    divBox.addEventListener("mousedown", () => {
-      divBox.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    });
+    divBox.addEventListener("mouseover", changeDivBackground);
+    divBox.addEventListener("mousedown", changeDivBackground);
   };
 };
+
+// const colorVariationHalfHalf = function (totalNumberOfSquareDivs) {
+//   const divBoxes = document.querySelectorAll(".divBox");
+//   for (let i = 0; i < (totalNumberOfSquareDivs / 2); i++) {
+//     divBoxes[i].style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+//   }
+// }
 
 const removeGridContainerFromCanvas = function () {
   const gridDivs = document.querySelectorAll(".divBox");
@@ -31,31 +52,35 @@ const removeGridContainerFromCanvas = function () {
 };
 
 const getUserInput = function (callback) {
-  let newGridSize = window.prompt("Please enter a number(between 0 and 100) of squares per side for the new grid.");
+  let newGridSize = window.prompt("Please enter a number(between 1 and 100) of squares per side for the new grid.");
   if (isNaN(newGridSize)) {
     alert("This is not a valid Number!")
-    getUserInput(createNewGridOFSquareDivs);
+    getUserInput(totalNumberOfSquareDivs);
   }
   else if (newGridSize === null) {
-    return callback(16);
+    // callback(4, createNewGridOFSquareDivs);
+    return;
   }
   else {
     if ((newGridSize > 0) && (newGridSize < 100)) {
-      callback(newGridSize);
+      callback(newGridSize, createNewGridOFSquareDivs);
     }
     else {
       alert("Your given value is not between 1 and 100!");
-      getUserInput(createNewGridOFSquareDivs);
+      getUserInput(totalNumberOfSquareDivs);
     }
   }
 };
 
-
-
 //initial grid layout
-createNewGridOFSquareDivs(16);
-const redrawCanvasButton = document.querySelector(".redrawCanvasButton");
-redrawCanvasButton.addEventListener("click", () => {
-  removeGridContainerFromCanvas();
-  getUserInput(createNewGridOFSquareDivs);
-});
+function initGrid() {
+  totalNumberOfSquareDivs(4, createNewGridOFSquareDivs);
+  const redrawCanvasButton = document.querySelector(".redrawCanvasButton");
+  redrawCanvasButton.addEventListener("click", () => {
+    removeGridContainerFromCanvas();
+    getUserInput(totalNumberOfSquareDivs);
+  });
+};
+
+initGrid();
+
